6a0294d036e634d37be24d821904039f51f10d84
Maidul Islam <34934101+vatia60@users.noreply.github.com>
2020-05-16 00:32:54 +0600

    seeder

--- app/Models/OrderProduct.php
+++ app/Models/OrderProduct.php
@@ -7,4 +7,16 @@ use Illuminate\Database\Eloquent\Model;
 class OrderProduct extends Model
 {
     protected $guarded = [];
+
+    public $timestamps = false;
+
+    public function order()
+    {
+        return $this->belongsTo(Order::class);
+    }
+
+    public function product()
+    {
+        return $this->belongsTo(Product::class);
+    }
 }
